0cfa15b7d088bdc4b43917620e7489d46e9165f7
Qian Wang <wangqian5730@gmail.com>
2021-05-21 15:05:53 +0800

    参数设置

--- modules/sample_mnmt/task.py
+++ modules/sample_mnmt/task.py
@@ -10,6 +10,20 @@ logger = logging.getLogger(__name__)
 
 @register_task("sample_mnmt")
 class SampledMultilingualTask(MultilingualTranslationTask):
+    @staticmethod
+    def add_args(parser):
+        MultilingualTranslationTask.add_args(parser)
+        parser.add_argument(
+            '--sample-method',
+            default='proportional',
+            choices=['temperature', 'proportional', 'uniform'])
+        parser.add_argument('--sample-temperature', default=5, type=int)
+
     def load_dataset(self, split, epoch=1, **kwargs):
         super(SampledMultilingualTask, self).load_dataset(split, epoch)
-        self.datasets[split] = MultilingualSampledDataset(self.datasets[split].datasets, self.datasets[split].eval_key)
+        self.datasets[split] = MultilingualSampledDataset(
+            self.datasets[split].datasets,
+            self.datasets[split].eval_key,
+            sample_method=self.args.sample_method,
+            temperature=self.args.sample_temperature
+        )
