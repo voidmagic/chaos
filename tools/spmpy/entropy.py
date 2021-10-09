0d44d2fe3064fa0280a510cba929350d2157aa93
Qian Wang <wangqian5730@gmail.com>
2021-10-09 22:35:21 +0800

    bpe: 计算语料熵

--- tools/spmpy/entropy.py
+++ tools/spmpy/entropy.py
@@ -2,6 +2,7 @@ import math
 import sys
 import collections
 
+norm = len(sys.argv) == 2 and sys.argv[1] == 'norm'
 
 counter = collections.Counter()
 for line in sys.stdin:
@@ -9,6 +10,9 @@ for line in sys.stdin:
     counter.update(line)
 
 total_word = sum(counter.values())
-entropy = [-(freq / total_word) * math.log(freq / total_word, 2) for word, freq in counter.items()]
+if norm:
+    entropy = [- 1 / len(word) * (freq / total_word) * math.log(freq / total_word, 2) for word, freq in counter.items()]
+else:
+    entropy = [- 1 * (freq / total_word) * math.log(freq / total_word, 2) for word, freq in counter.items()]
 entropy = round(sum(entropy), 4)
 print(entropy)
