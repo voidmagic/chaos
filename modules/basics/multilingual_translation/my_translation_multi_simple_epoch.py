72bbe95b517e450349f09ab5ab393ef15ffcdaff
qian <qian.wng@nlpr.ia.ac.cn>
2021-12-24 12:50:59 +0800

    basic multi simple epoch

--- modules/basics/multilingual_translation/my_translation_multi_simple_epoch.py
+++ modules/basics/multilingual_translation/my_translation_multi_simple_epoch.py
@@ -1,9 +1,11 @@
 import datetime
 import logging
 
+import torch
 from fairseq.data import data_utils
 from fairseq.data.multilingual.multilingual_data_manager import MultilingualDatasetManager
 from fairseq.data.multilingual.sampling_method import SamplingMethod
+from fairseq.optim import AMPOptimizer
 from fairseq.tasks import register_task
 from fairseq.tasks.translation_multi_simple_epoch import TranslationMultiSimpleEpochTask
 from fairseq.utils import FileContentsAction
@@ -49,7 +51,21 @@ class MyTranslationMultiSimpleEpochTask(TranslationMultiSimpleEpochTask):
         return loss, sample_size, logging_output
 
     def train_step(self, sample, model, criterion, optimizer, update_num, ignore_grad=False):
-        loss, sample_size, logging_output = super(MyTranslationMultiSimpleEpochTask, self).train_step(
-            sample, model, criterion, optimizer, update_num, ignore_grad
-        )
-        return loss, sample_size, logging_output
+
+        def reduce_logging_output(_logging_output):
+            return {
+                key: value.sum() if isinstance(value, torch.Tensor) else value
+                for key, value in _logging_output.items()
+            }
+
+        model.train()
+        model.set_num_updates(update_num)
+        loss, sample_size, logging_output = criterion(model, sample, reduce=False)
+        loss = loss.sum()
+        if ignore_grad:
+            loss *= 0
+        optimizer.backward(loss)
+        return loss, sample_size, reduce_logging_output(logging_output)
+
+    def infer_task(self, sample):
+        source_tokens = sample["net_input"]["src_tokens"]
