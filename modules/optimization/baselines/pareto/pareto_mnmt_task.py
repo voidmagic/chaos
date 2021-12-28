14ddfad0e772318504174d177f43f285da99b59c
wangqian <qian.wang@nlpr.ia.ac.cn>
2021-12-28 12:22:34 +0800

    pareto add args and freeze

--- modules/optimization/baselines/pareto/pareto_mnmt_task.py
+++ modules/optimization/baselines/pareto/pareto_mnmt_task.py
@@ -1,3 +1,9 @@
+"""
+Support:
+    pareto MTL with min norm solver.
+    freeze part of the model, currently last decoder layer.
+"""
+
 import collections
 import logging
 
@@ -13,8 +19,23 @@ logger = logging.getLogger(__name__)
 
 @register_task("pareto_mnmt")
 class ParetoMultilingualNeuralMachineTranslationTask(TranslationMultiSimpleEpochTask):
-    alpha, smooth = None, 0.1
-    last_update_or_start, update_interval = 10000, 200
+    def __init__(self, args, langs, dicts, training):
+        super(ParetoMultilingualNeuralMachineTranslationTask, self).__init__(args, langs, dicts, training)
+        self.alpha = None
+        self.smooth = args.smooth_alpha
+        self.last_update_or_start = args.start_update_alpha
+        self.update_interval = args.update_interval_alpha
+        self.eval_param_key = args.eval_key_alpha
+
+    @staticmethod
+    def add_args(parser):
+        """Add task-specific arguments to the parser."""
+        # fmt: off
+        parser.add_argument('--smooth-alpha', default=0.1, type=float, help='smooth alpha to avoid zero weight.')
+        parser.add_argument('--start-update-alpha', default=10000, type=int, help='step to start update alpha')
+        parser.add_argument('--update-interval-alpha', default=200, type=int, help='alpha update interval')
+        parser.add_argument('--eval-key-alpha', default=None, type=str, help='use param with name of eval key to eval alpha')
+        TranslationMultiSimpleEpochTask.add_args(parser)
 
     def train_step(self, sample, model, criterion, optimizer, update_num, ignore_grad=False):
         if update_num > self.last_update_or_start and update_num % self.update_interval == 0:
@@ -38,6 +59,13 @@ class ParetoMultilingualNeuralMachineTranslationTask(TranslationMultiSimpleEpoch
         return loss, sample_size, {
             key: value.sum() if isinstance(value, torch.Tensor) else value for key, value in logging_output.items()
         }
+
+    def build_model(self, args):
+        model = super(ParetoMultilingualNeuralMachineTranslationTask, self).build_model(args)
+        for name, param in model.named_parameters():
+            if self.eval_param_key is not None and not name.startswith(self.eval_param_key):
+                param.requires_grad = False
+        return model
 
     def reset_alpha(self, model, criterion):
 
