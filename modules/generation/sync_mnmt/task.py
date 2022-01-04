ddec241662e494258c8442d900115332377847b6
wangqian <qian.wang@nlpr.ia.ac.cn>
2022-01-04 19:34:23 +0800

    roll back sync mnmt

--- modules/generation/sync_mnmt/task.py
+++ modules/generation/sync_mnmt/task.py
@@ -2,7 +2,6 @@ import os
 
 from fairseq.data import indexed_dataset, data_utils, PrependTokenDataset
 from fairseq.tasks import register_task
-from fairseq.tasks.translation_multi_simple_epoch import TranslationMultiSimpleEpochTask
 
 from modules.basics.google_mnmt.google_mnmt_task import GoogleMultilingualTranslationTask
 from .utils.dataset import MultiParallelDataset
@@ -23,21 +22,21 @@ class Config:
 
 
 @register_task("sync_mnmt")
-class SyncTranslationTask(TranslationMultiSimpleEpochTask):
+class SyncTranslationTask(GoogleMultilingualTranslationTask):
 
     @classmethod
     def add_args(cls, parser):
-        TranslationMultiSimpleEpochTask.add_args(parser)
+        GoogleMultilingualTranslationTask.add_args(parser)
         parser.add_argument('--manner', default='tanh', type=str)
         parser.add_argument('--tanh-weight', default=0.1, type=float)
         parser.add_argument('--non-proj', action='store_true')
         parser.add_argument('--weight-path', default=None, type=str)
         parser.add_argument('--fusion-last', action='store_true')
-        parser.add_argument('--train-cla', action='store_true')
 
     @classmethod
     def setup_task(cls, args, **kwargs):
         task = super(SyncTranslationTask, cls).setup_task(args)
+        task.args = args
         Config.n_lang = len(args.lang_pairs)
         Config.manner = args.manner
         Config.tanh_weight = args.tanh_weight
@@ -47,7 +46,6 @@ class SyncTranslationTask(TranslationMultiSimpleEpochTask):
         return task
 
     def load_dataset(self, split, **kwargs):
-
         def load_data(src, tgt):
             if indexed_dataset.dataset_exists(os.path.join(self.args.data, '{}.{}-{}.{}'.format(split, src, tgt, src)), None):
                 prefix = os.path.join(self.args.data, '{}.{}-{}.'.format(split, src, tgt))
@@ -56,28 +54,27 @@ class SyncTranslationTask(TranslationMultiSimpleEpochTask):
             else:
                 raise FileNotFoundError(os.path.join(self.args.data, '{}.{}-{}.*'.format(split, tgt, src)))
 
-            src_raw_dataset = data_utils.load_indexed_dataset(prefix + src, self.source_dictionary)
-            tgt_raw_dataset = data_utils.load_indexed_dataset(prefix + tgt, self.target_dictionary)
-            tgt_prepend_dataset = PrependTokenDataset(tgt_raw_dataset, self.source_dictionary.index('__{}__'.format(tgt)))
+            src_raw_dataset = data_utils.load_indexed_dataset(prefix + src, self.src_dict)
+            tgt_raw_dataset = data_utils.load_indexed_dataset(prefix + tgt, self.tgt_dict)
+            tgt_prepend_dataset = PrependTokenDataset(tgt_raw_dataset, self.src_dict.index('__2<{}>__'.format(tgt)))
             return src_raw_dataset, tgt_prepend_dataset
 
         if split == 'test' and self.args.source_lang != self.args.target_lang:
             src_dataset, tgt_dataset = load_data(self.args.source_lang, self.args.target_lang)
-            self.datasets[split] = MultiParallelDataset(src_dataset, [tgt_dataset], self.source_dictionary)
-            Config.infer_target = self.source_dictionary.index('__{}__'.format(self.args.target_lang))
+            self.datasets[split] = MultiParallelDataset(src_dataset, [tgt_dataset], self.src_dict)
+            Config.infer_target = self.src_dict.index('__2<{}>__'.format(self.args.target_lang))
             for _, tgt in self.args.lang_pairs:
-                Config.lang_idx.add(self.source_dictionary.index('__{}__'.format(tgt)))
+                Config.lang_idx.add(self.src_dict.index('__2<{}>__'.format(tgt)))
             return
 
         src_datasets = []
         tgt_datasets = []
         for lang_pair in self.args.lang_pairs:
-            src, tgt = lang_pair.split('-')
-            src_dataset, tgt_dataset = load_data(src, tgt)
+            src_dataset, tgt_dataset = load_data(lang_pair[0], lang_pair[1])
             src_datasets.append(src_dataset)
             tgt_datasets.append(tgt_dataset)
 
-        self.datasets[split] = MultiParallelDataset(src_datasets[0], tgt_datasets, self.source_dictionary)
+        self.datasets[split] = MultiParallelDataset(src_datasets[0], tgt_datasets, self.src_dict)
 
     def build_generator(self, models, args, seq_gen_cls=None, extra_gen_cls_kwargs=None, extra=None):
         return super(SyncTranslationTask, self).build_generator(models, args, SequenceGenerator, extra_gen_cls_kwargs)
