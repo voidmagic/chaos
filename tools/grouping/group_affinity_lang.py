a77d96c025d51d503318ce31b76710e2cfc231cb
wangqian <qian.wang@nlpr.ia.ac.cn>
2022-03-28 09:15:32 +0800

    group embedding or affinity language

---
+++ tools/grouping/group_affinity_lang.py
@@ -0,0 +1,63 @@
+import tqdm
+import numpy as np
+
+root_path = "/home/qwang/027-optim/003-models/210-multilingual-ted/diverse/o2m/proportional"
+log_path = f"{root_path}/log.txt"
+
+
+all_languages = set()
+
+with open(log_path) as f:
+    for line in f:
+        if "fairseq.data.multilingual.multilingual_data_manager" not in line or "src_langtok" not in line:
+            continue
+        if "| train_inner |" in line:
+            break
+        lang_key = line.split()[7]
+        all_languages.add(lang_key)
+
+
+def remove_common(a_list):
+    i, j = 0, len(a_list[0])
+    while i < j:
+        if len(set([s[i] for s in a_list])) > 1:
+            break
+        i += 1
+    while i < j:
+        if len(set([s[j-1] for s in a_list])) > 1:
+            break
+        j -= 1
+
+    a_list = [s[i:j] for s in a_list]
+    return a_list, i, j
+
+
+all_languages, start, end = remove_common(list(all_languages))
+
+affinity_matrix = {lang1: {lang2: [] for lang2 in all_languages} for lang1 in all_languages}
+
+
+num_steps, step_start, step_end = 0, 17500, 25000
+with open(log_path) as f:
+    for line in tqdm.tqdm(f.readlines()):
+        if "Affinity" not in line or "modules.optimization.affinity.affinity_task" not in line:
+            continue
+        num_steps += 1
+        if step_start < num_steps < step_end:
+            languages = line.strip().split(" | ")[4].strip("['] ").split("', '")
+            languages = [lang.strip('_') for lang in languages]
+            target = line.strip().split(" | ")[6][start:end]
+            affinity = float(line.strip().split(" | ")[7])
+            for lang in languages:
+                affinity_matrix[target][lang].append(affinity)
+
+print("\t\t\t" + "\t\t\t\t".join(sorted(affinity_matrix.keys())))
+for key1 in sorted(affinity_matrix.keys()):
+    print(key1, end="    ")
+    for key2 in sorted(affinity_matrix[key1].keys()):
+        affinity_matrix[key1][key2] = np.mean(affinity_matrix[key1][key2] or [0])
+        print("%.8f" % float(affinity_matrix[key1][key2]), end="\t\t")
+    print()
+
+
+
---
+++ tools/grouping/group_embed.py
@@ -0,0 +1,39 @@
+import collections
+
+import torch
+import re
+from scipy.cluster.hierarchy import dendrogram, linkage, cut_tree
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+root_path = "/home/qwang/027-optim/003-models/300-multilingual-opus/o2m"
+model_path = f"{root_path}/checkpoint_average.pt"
+log_path = f"{root_path}/log.txt"
+
+model = torch.load(model_path)
+embedding = model["model"]["encoder.embed_tokens.weight"]
+
+key_embedding = {}
+with open(log_path) as f:
+    for line in f:
+        if "fairseq.data.multilingual.multilingual_data_manager" not in line or "src_langtok" not in line:
+            continue
+        key = re.findall(r".*main:(.*) src.*", line)[0]
+        index = re.findall(r".*src_langtok: (.*);.*", line)[0]
+        key_embedding[key] = embedding[int(index)].tolist()
+
+Z = linkage(np.array(list(key_embedding.values())), 'complete', metric="cosine")
+fig = plt.figure(figsize=(15, 10))
+dn = dendrogram(Z, labels=np.array(list(key_embedding.keys())))
+plt.show()
+
+clusters = cut_tree(Z, height=0.8)
+langs = list(key_embedding.keys())
+
+language_clusters = collections.defaultdict(list)
+for cls, lang in zip(clusters.T[0], langs):
+    language_clusters[cls].append(lang)
+
+for key, value in language_clusters.items():
+    print(key, value)
